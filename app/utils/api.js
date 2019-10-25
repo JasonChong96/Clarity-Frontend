3b60ab7ba0eecb2feeb8cadfa92a4d2ebf66b22f
JasonChong96 <jasontjakra@gmail.com>
2019-10-26 00:37:34 +0800

    API Helpers

---
+++ app/utils/api.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import router from './router/router';
+
+const ACCESS_TOKEN_KEY = 'access_token';
+
+const oraAxios = axios.create({
+  baseURL: 'localhost', // URL here
+});
+
+function getAccessToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
+
+function setAccessToken(token) {
+  localStorage.setItem(ACCESS_TOKEN_KEY, token);
+}
+
+oraAxios.interceptors.request.use(
+  config => {
+    const token = localStorageService.getAccessToken();
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token;
+    }
+    // config.headers['Content-Type'] = 'application/json';
+    return config;
+  },
+  error => {
+    Promise.reject(error);
+  },
+);
+
+oraAxios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  function(error) {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      originalRequest.url.endsWith('refresh')
+    ) {
+      router.push('/');
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      return axios.post('/refresh', {}).then(res => {
+        if (res.status === 201) {
+          setAccessToken(res.data.access_token);
+          axios.defaults.headers.common['Authorization'] =
+            'Bearer ' + getAccessToken();
+          return axios(originalRequest);
+        }
+      });
+    }
+    return Promise.reject(error);
+  },
+);
+
+export function post(url, payload, callback, errorHandler) {
+  oraAxios
+    .post(url, payload)
+    .then(response => callback(response))
+    .catch(error => errorHandler(error));
+}
+
+export function get(url, callback, errorHandler) {
+  oraAxios
+    .get(url)
+    .then(response => callback(response))
+    .catch(error => errorHandler(error));
+}
+
+export function patch(url, payload, callback, errorHandler) {
+  oraAxios
+    .patch(url, payload)
+    .then(response => callback(response))
+    .catch(error => errorHandler(error));
+}
+
+export function put(url, payload, callback, errorHandler) {
+  oraAxios
+    .put(url, payload)
+    .then(response => callback(response))
+    .catch(error => errorHandler(error));
+}
