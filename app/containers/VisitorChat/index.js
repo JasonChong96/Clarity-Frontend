8e60194987f204156e2c12a943e05de3c97d91b4
Son <son@fr0st.local>
2019-11-03 14:12:33 +0800

    Change API URL to the Ora's domain

--- app/containers/VisitorChat/index.js
+++ app/containers/VisitorChat/index.js
@@ -97,7 +97,8 @@ export function VisitorChat({
   const [showSignUp, setShowSignUp] = useState(false);
   const [showSignUpForLogOut, setShowSignUpForLogOut] = useState(false);
   function connectSocket() {
-    const socket = socketIOClient('157.230.253.130:8080', {
+    // const socket = socketIOClient('157.230.253.130:8080', {
+    const socket = socketIOClient('https://api.chatwithora.com', {
       // const socket = socketIOClient('http://192.168.1.141:8080', {
       transportOptions: {
         polling: {
