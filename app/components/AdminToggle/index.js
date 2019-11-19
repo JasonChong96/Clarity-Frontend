e804d9285bd48137336a50cd0b14a18ec062f3ca
TravisCJY <travisching007@gmail.com>
2019-11-20 03:09:28 +0800

    Add AdminToggle Component

---
+++ app/components/AdminToggle/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * AdminToggle
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function AdminToggle() {
+  return <div />;
+}
+
+AdminToggle.propTypes = {};
+
+export default memo(AdminToggle);
---
+++ app/components/AdminToggle/tests/index.test.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Tests for AdminToggle
+ *
+ * @see https://github.com/react-boilerplate/react-boilerplate/tree/master/docs/testing
+ *
+ */
+
+import React from 'react';
+import { render } from 'react-testing-library';
+// import 'jest-dom/extend-expect'; // add some helpful assertions
+
+import AdminToggle from '../index';
+
+describe('<AdminToggle />', () => {
+  it('Expect to not log errors in console', () => {
+    const spy = jest.spyOn(global.console, 'error');
+    render(<AdminToggle />);
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('Expect to have additional unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+
+  /**
+   * Unskip this test to use it
+   *
+   * @see {@link https://jestjs.io/docs/en/api#testskipname-fn}
+   */
+  it.skip('Should render and match the snapshot', () => {
+    const {
+      container: { firstChild },
+    } = render(<AdminToggle />);
+    expect(firstChild).toMatchSnapshot();
+  });
+});
